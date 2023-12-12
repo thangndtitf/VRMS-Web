4c191043fc50096e41b00826005bf86d295a11fb
NguyenThang <thangndtitf@gmail.com>
2023-12-12 23:58:54 +0700

    Thang commit for fix the input filed

--- src/components/textinput/passworldinput/PassworldInput.js
+++ src/components/textinput/passworldinput/PassworldInput.js
@@ -2,10 +2,31 @@
 import { Component } from 'react';
 import './PassworldInput.css'
 class PassworldInput extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            isFocus : false
+        }
+        this.clickIsOnFocus = this.clickIsOnFocus.bind(this);
+    }
+
+    clickIsOnFocus(){
+        this.setState(prevState => ({
+            isFocus : !prevState.isFocus
+        }))
+    }
+
     render(){
         return (
             <div className='input_passworld_container'>
-                <input type="password" className="input_passworld_filed" placeholder="Your password"/>
+                <input 
+                    type="password" 
+                    className="input_passworld_filed" 
+                    placeholder={this.state.isFocus? '':' Please input your Password' }
+                    onFocus={()=>this.clickIsOnFocus()}
+                    onBlur={()=>this.clickIsOnFocus()}
+                />
             </div>
         );
     }
--- src/components/textinput/userNameInput/UserNameInput.js
+++ src/components/textinput/userNameInput/UserNameInput.js
@@ -3,10 +3,31 @@ import React, { Component } from "react";
 import './UserNameInput.css'
 
 class UserNameInput extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            isFocus : false
+        }
+        this.clickIsOnFocus = this.clickIsOnFocus.bind(this);
+    }
+
+    clickIsOnFocus(){
+        this.setState(prevState => ({
+            isFocus : !prevState.isFocus
+        }))
+    }
+
     render(){
         return (
             <div className="usernamecontainer" >
-                <input type="text" className="usernamecontainer_input"  placeholder="User Name " />
+                <input 
+                    type="text" 
+                    className="usernamecontainer_input"  
+                    placeholder={this.state.isFocus ? '' : ' Please input your User Name'}
+                    onFocus={()=> this.clickIsOnFocus()}
+                    onBlur={()=> this.clickIsOnFocus()}
+                />
             </div>
         );
     }
